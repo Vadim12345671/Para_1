5803fdad082957c4895e810c5981eccb786bae42
Vadim12345671 <Vadimich27112017@gmail.com>
2022-11-20 11:14:36 +0200

    dz6

--- dz_6.py
+++ dz_6.py
@@ -1,14 +1,28 @@
 result = []
+
 def divider(a, b):
     if a < b:
-        raise ValueError
+        raise firsterror()
     if b > 100:
-        raise IndexError
-    return a/b
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+        raise seconderror()
+    return a / b
 
-for key in data:
-    res = divider(key, data[key])
-    result.append(res)
 
+class firsterror(Exception):
+    def __str__(self):
+        return "Number a must be bigger than b"
+
+
+class seconderror(Exception):
+    def __str__(self):
+        return "Number b can't be bigger than 100"
+
+
+try:
+    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+    for key in data:
+        res = divider(key, data[key])
+        result.append(res)
+except TypeError as error:
+    print(error)
 print(result)
